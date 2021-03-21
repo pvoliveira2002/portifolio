e4166bad983c646e6e28307f721b036fa3a88d5a
pvoliveira2002 <pvoliveira2002@gmail.com>
2021-03-21 18:48:39 -0300

    style.css
    eu tive que modificar o display que antes estava flex para initial, para que ele só aparecesse depois quando o teclado passasse encima.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 /*quando acontecer o evento de click ele vai executar essa funçao*/
-document.querySelector(".hamburguer").addEventListener("click", ()=>
+document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
     /*se tiver a classe show-menu ele tira se nao tiver ele coloca*/
